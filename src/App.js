dd32f3d4d3fb62d38c9803f5acd292043343c62f
julio Garcia <garcia717@gmail.com>
2023-02-20 10:32:17 -0300

    primera parte clase 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hola Mundo!</h1>
+  return ( 
+    
+  <div className = "App" >
+    <Navbar />
+    <Footer />
     </div>
   );
 }
